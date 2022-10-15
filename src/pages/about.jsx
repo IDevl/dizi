b7ac53ead29e97c0e1e4187a47bb8a6754eeca80
lDevl <jbdelacruz411@gmail.com>
2022-10-16 02:12:09 +0800

    change pic

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMobileScreenButton } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import profilepic from "./img/profilepic.jpg";
 
 export default function About() {
   return (
@@ -10,10 +11,10 @@ export default function About() {
 
         <div class="lg:hidden">
           <div class="w-full mb-6 lg:mb-0 mx-auto relative bg-white text-center dark:bg-[#111111] px-6 rounded-[20px] mt-[180px] md:mt-[220px] lg:mt-0">
-            <img src="https://bostamihtml.ibthemespro.com/images/about/avatar.jpg" class="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px]" alt="about" />
+            <img src={profilepic} class="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px] object-cover object-top" />
             <div class="pt-[100px] pb-8">
-              <h2 class="mt-6 mb-1 text-[26px] font-semibold dark:text-white"> Monalisa Ashley</h2>
-              <h3 class="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6]">Ui/Ux Designer </h3>
+              <h2 class="mt-6 mb-1 text-[26px] font-semibold dark:text-white"> Jefferson Dela Cruz </h2>
+              <h3 class="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6]"> Web Developer </h3>
 
               <div class="flex justify-center space-x-3">
                 <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
@@ -26,11 +27,13 @@ export default function About() {
               <div class="p-7 rounded-2xl mt-7 bg-[#F3F6F6] dark:bg-[#1D1D1D]">
                 <div class="flex border-b border-[#E3E3E3] dark:border-[#3D3A3A] pb-2.5">
                   <span class="socialbtn bg-white dark:bg-black text-[#E93B81] shadow-md">
-                    <FontAwesomeIcon icon={faMobileScreenButton} />
+                    <a href="tel: +639384571460" target="_blank" rel="noreferrer">
+                      <FontAwesomeIcon icon={faMobileScreenButton} />
+                    </a>
                   </span>
                   <div class="text-left ml-2.5">
                     <p class="text-xs text-[#44566C] dark:text-[#A6A6A6]"> Phone </p>
-                    <p class="dark:text-white">+123 456 7890</p>
+                    <p class="dark:text-white">+63 938 4571 460</p>
                   </div>
                 </div>
               </div>
@@ -41,8 +44,8 @@ export default function About() {
         <div class="lg:grid grid-cols-12 md:gap-10 pt-4 md:pt-[30px] items-center hidden">
           <div class="col-span-12 space-y-2.5">
             <div class="lg:mr-16">
-              <p class="text-[#44566c] dark:text-color-910 leading-7"> I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs. </p>
-              <p class="text-[#44566c] leading-7 mt-2.5 dark:text-color-910"> My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies. </p>
+              <p class="text-[#b3b3b3] leading-7"> I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs. </p>
+              <p class="text-[#b3b3b3] leading-7 mt-2.5"> My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies. </p>
             </div>
           </div>
         </div>
