a0055eba5d8c5819e82b92fa3bf641fae78ffa07
lDevl <jbdelacruz411@gmail.com>
2022-10-17 01:48:48 +0800

    mapping educt

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,6 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptop, faMobile, faGraduationCap, faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { faUncharted } from "@fortawesome/free-brands-svg-icons";
 
+const education = [
+  {
+    year: "2018-2022",
+    name: "BS Information Technology",
+    location: "Polytechnic University of the Philippines",
+  },
+  {
+    year: "2016-2018",
+    name: "STEM",
+    location: "STI College",
+  },
+];
+
+const experiences = [
+  {
+    year: "2021-2022",
+    name: "Web Development",
+    location: "Exceed Digital Solutions",
+  },
+];
+
 export default function About() {
   return (
     <>
@@ -24,28 +45,26 @@ export default function About() {
 
       <div class="pb-12 px-2 sm:px-5 md:px-10 lg:px-14">
         <h3 class="text-[2.2rem] text-white font-semibold pb-5"> What I do! </h3>
-        <div class="grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2">
+        <div class="grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-2">
           <div class="flex items-start gap-4 p-6 rounded-xl border-[1px] border-[#383838] bg-transparent">
-            <FontAwesomeIcon class="text-[#AF54D2] max-h-[100px]" icon={faLaptop} />
+            <FontAwesomeIcon class="text-[#AF54D2] max-h-[35px]" icon={faLaptop} />
             <div class="space-y-2">
               <h3 class="text-white text-[1.25rem] font-semibold"> Web Development </h3>
-              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
+              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem Ipsum </p>
             </div>
           </div>
-
           <div class="flex items-start gap-4 p-6 rounded-xl border-[1px] border-[#383838] bg-transparent">
-            <FontAwesomeIcon class="text-[#A1760A] max-h-[100px]" icon={faMobile} />
+            <FontAwesomeIcon class="text-[#AF54D2] max-h-[35px]" icon={faLaptop} />
             <div class="space-y-2">
-              <h3 class="text-white text-[1.25rem] font-semibold"> App Development </h3>
-              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
+              <h3 class="text-white text-[1.25rem] font-semibold"> Web Development </h3>
+              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem Ipsum </p>
             </div>
           </div>
-
           <div class="flex items-start gap-4 p-6 rounded-xl border-[1px] border-[#383838] bg-transparent">
-            <FontAwesomeIcon class="text-[#1F83D2] max-h-[100px]" icon={faUncharted} />
+            <FontAwesomeIcon class="text-[#AF54D2] max-h-[35px]" icon={faLaptop} />
             <div class="space-y-2">
-              <h3 class="text-white text-[1.25rem] font-semibold"> Software Development </h3>
-              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
+              <h3 class="text-white text-[1.25rem] font-semibold"> Web Development </h3>
+              <p class="leading-8 text-gray-lite text-[#A6A6A6] text-[0.9rem]"> Lorem Ipsum </p>
             </div>
           </div>
         </div>
@@ -54,26 +73,31 @@ export default function About() {
       <div class="container py-12 px-2 sm:px-5 md:px-10 lg:px-14 bg-black">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-6">
           <div>
-            <div class="flex items-center space-x-3 mb-4">
+            <div class="flex items-center space-x-3 mb-5">
               <FontAwesomeIcon class="text-[#df0000] max-h-[30px]" icon={faGraduationCap} />
               <h4 class="text-[1.6rem] text-white font-medium"> Education </h4>
             </div>
-            <div class="bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg border-[#383838] border-[1px]">
-              <span class="text-[1rem] text-[#b7b7b7]">2016-2022</span>
-              <h3 class="text-[1.25rem] text-white"> BS Information Technology </h3>
-              <p class="text-[0.9rem] text-[#b7b7b7]"> Polytechnic University of the Philippines </p>
-            </div>
+            {education.map((education) => (
+              <div class="mt-5 bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg border-[#383838] border-[1px]">
+                <span class="text-[1rem] text-[#b7b7b7]"> {education.year} </span>
+                <h3 class="text-[1.25rem] text-white"> {education.name} </h3>
+                <p class="text-[0.9rem] text-[#b7b7b7]"> {education.location} </p>
+              </div>
+            ))}
+            ;
           </div>
           <div>
-            <div class="flex items-center space-x-3 mb-4">
+            <div class="flex items-center space-x-3 mb-5">
               <FontAwesomeIcon class="text-[#AF54D2] max-h-[30px]" icon={faBriefcase} />
               <h4 class="text-[1.6rem] text-white font-medium"> Experience </h4>
             </div>
-            <div class="bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg border-[#383838] border-[1px]">
-              <span class="text-[1rem] text-[#b7b7b7]">2021-2022</span>
-              <h3 class="text-[1.25rem] text-white"> Web Developer </h3>
-              <p class="text-[0.9rem] text-[#b7b7b7]"> Exceed Digital Solutions </p>
-            </div>
+            {experiences.map((experiences) => (
+              <div class="mt-5 bg-transparent py-4 pl-5 pr-3 space-y-2 rounded-lg border-[#383838] border-[1px]">
+                <span class="text-[1rem] text-[#b7b7b7]"> {experiences.year} </span>
+                <h3 class="text-[1.25rem] text-white"> {experiences.name} </h3>
+                <p class="text-[0.9rem] text-[#b7b7b7]"> {experiences.location} </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
