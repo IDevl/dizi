40f299d53ae5bf1a345515956d8ab963a3554ce4
lDevl <jbdelacruz411@gmail.com>
2022-10-18 00:12:05 +0800

    a

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ export default function App() {
           <div class="lg:rounded-2xl bg-[#111111]">
             <Routes>
               {/* <Route index element={<About />} /> */}
-              <Route path="/" element={<About />} />
-              <Route path="/resume" element={<Resume />} />
+              <Route path="./" element={<About />} />
+              <Route path="./resume" element={<Resume />} />
             </Routes>
             <Footer />
           </div>
