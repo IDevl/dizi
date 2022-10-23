d8900e31adbe99cda049098b09d451c84ce24ed2
lDevl <jbdelacruz411@gmail.com>
2022-10-23 15:39:53 +0800

    project images

--- src/pages/about/profile.jsx
+++ src/pages/about/profile.jsx
@@ -7,7 +7,7 @@ export default function Profile() {
     <>
       <div className="w-full mb-6 lg:mb-0 mx-auto relative text-center bg-[#111111] px-6 rounded-[20px] mt-[180px] md:mt-[220px] lg:mt-0">
         <a href="./">
-          <img src={process.env.PUBLIC_URL + "/about/profile-pic.jpg"} className="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px] object-cover object-top" alt="Picture_Profile" />
+          <img src={process.env.PUBLIC_URL + "/img/about/profile-pic.jpg"} className="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px] object-cover object-top" alt="Picture_Profile" />
         </a>
         <div className="pt-[100px] pb-8">
           <h2 className="mt-6 mb-1 text-[26px] font-semibold text-white"> Jefferson Dela Cruz </h2>
