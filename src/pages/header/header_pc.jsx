406c38bd4333564fdca4b7eca2a1443def626d63
lDevl <jbdelacruz411@gmail.com>
2022-10-31 23:01:01 +0800

    headerpc - fix darkmode toggle

--- src/pages/header/header_pc.jsx
+++ src/pages/header/header_pc.jsx
@@ -1,26 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import classnames from "classnames";
 import { useEffect, useState } from "react";
 
 export default function HeaderPC() {
   const [theme, setTheme] = useState(localStorage.theme);
 
   useEffect(() => {
-    if (theme === 'dark') {
-      localStorage.setItem('theme', 'dark');
-      document.documentElement.classList.add('dark');
-    }
-    else {
-      localStorage.setItem('theme', 'light');
-      document.documentElement.classList.remove('dark');
-    }
+    setTheme(localStorage.theme);
+  }, []);
 
+  useEffect(() => {
+    console.log(theme);
+    if (theme === "dark") {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
   }, [theme]);
   return (
     <div className="bg-white lg:bg-transparent dark:bg-[#111111] dark:lg:bg-[#252527] container top-0 left-0 right-0 z-[9999] h-[120px] flex justify-end items-center block px-6">
-      <button className="flex items-center justify-center text-xl text-black dark:text-white rounded-[100em] w-[50px] h-[50px] bg-white dark:bg-[#4D4D4D] shadow-black shadow-sm" onClick={() => theme === 'light' ? setTheme('dark') : setTheme('light')}>
-        <FontAwesomeIcon icon={faMoon} className="hidden dark:block"/>
+      <button className="flex items-center justify-center text-xl text-black dark:text-white rounded-[100em] w-[50px] h-[50px] bg-white dark:bg-[#4D4D4D] shadow-black shadow-sm" onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}>
+        <FontAwesomeIcon icon={faMoon} className="hidden dark:block" />
         <FontAwesomeIcon icon={faSun} className="block dark:hidden" />
       </button>
     </div>
