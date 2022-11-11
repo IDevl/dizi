f1ed1a3b116c717cf36406ca329b90f1884bcef6
IDevl <jbdelacruz411@gmail.com>
2022-11-11 19:36:02 +0800

    mobile nav - hide when scroll down

--- src/pages/header/header_mobile.jsx
+++ src/pages/header/header_mobile.jsx
@@ -25,46 +25,44 @@ export default function HeaderMobile() {
       }
 
       prevScrollY.current = currentScrollY;
-      console.log(currentScrollY)
-      
     };
 
-    window.addEventListener("scroll", handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll);
 
     return () => window.removeEventListener("scroll", handleScroll);
   }, [scrollDown]);
 
   return (
-    <div id="nav-mobile" className="fixed bottom-0 left-0 right-0 z-[9999] block lg:hidden duration-[0.2s]">
-      <div className="w-full flex items-center justify-center gap-[1.2em] px-4 py-5 bg-white dark:bg-black drop-shadow-[0_-2.5px_5px_rgba(0,0,0,0.1)]">
-        <Link className={classnames("menu-item text-[1.1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")} to="./">
-          <span className="text-md mb-1">
+    <div id="nav-mobile" className="fixed bottom-0 left-0 right-0 z-[9999] block lg:hidden duration-[0.1s]">
+      <div className="w-full flex items-center justify-center gap-[1em] px-3 py-4 bg-white dark:bg-black drop-shadow-[0_-2.5px_5px_rgba(0,0,0,0.1)]">
+        <Link className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")} to="./">
+          <span className="text-[1rem] mb-1">
             <FontAwesomeIcon icon={faUser} className="flex items-center justify-center" />
-          </span>{" "}
+          </span>{"About"}
         </Link>
         <Link
-          className={classnames("menu-item text-[1.2rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/resume" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/resume" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./resume"
         >
-          <span className="text-md mb-1">
+          <span className="text-[1rem] mb-1">
             <FontAwesomeIcon icon={faFileLines} className="flex items-center justify-center" />
-          </span>{" "}
+          </span>{"Resume"}
         </Link>
         <Link
-          className={classnames("menu-item text-[1.2rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/projects" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/projects" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./projects"
         >
-          <span className="text-md mb-1">
+          <span className="text-[1rem] mb-1">
             <FontAwesomeIcon icon={faBriefcase} className="flex items-center justify-center" />
-          </span>{" "}
+          </span>{"Projects"}
         </Link>
         <Link
-          className={classnames("menu-item text-[1.2rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/certificates" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/certificates" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./certificates"
         >
-          <span className="text-md mb-1">
+          <span className="text-[1rem] mb-1">
             <FontAwesomeIcon icon={faCertificate} className="flex items-center justify-center" />
-          </span>{" "}
+          </span>{"Certificates"}
         </Link>
       </div>
     </div>
