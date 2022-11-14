3f7b8c6ac5e06a23f562497dd3859f7f02b48ba6
IDevl <jbdelacruz411@gmail.com>
2022-11-14 14:21:18 +0800

    mobile header revisions

--- src/pages/header/header_mobile.jsx
+++ src/pages/header/header_mobile.jsx
@@ -35,13 +35,13 @@ export default function HeaderMobile() {
   return (
     <div id="nav-mobile" className="fixed bottom-0 left-0 right-0 z-[9999] block lg:hidden duration-[0.1s]">
       <div className="w-full flex items-center justify-center gap-[0.5em] p-3 bg-white dark:bg-black drop-shadow-[0_-2.5px_5px_rgba(0,0,0,0.1)]">
-        <Link className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")} to="./">
+        <Link className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] dark:bg-[#1D1D1D] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)]", location.pathname === "/" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")} to="./">
           <span className="text-[1rem] mb-1">
             <FontAwesomeIcon icon={faUser} className="flex items-center justify-center" />
           </span>{"About"}
         </Link>
         <Link
-          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/resume" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] dark:bg-[#1D1D1D] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)]", location.pathname === "/resume" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./resume"
         >
           <span className="text-[1rem] mb-1">
@@ -49,7 +49,7 @@ export default function HeaderMobile() {
           </span>{"Resume"}
         </Link>
         <Link
-          className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/projects" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-[1rem] text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] dark:bg-[#1D1D1D] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)]", location.pathname === "/projects" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./projects"
         >
           <span className="text-[1rem] mb-1">
@@ -57,7 +57,7 @@ export default function HeaderMobile() {
           </span>{"Projects"}
         </Link>
         <Link
-          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname === "/certificates" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] dark:bg-[#1D1D1D] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)]", location.pathname === "/certificates" && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./certificates"
         >
           <span className="text-[1rem] mb-1">
@@ -65,7 +65,7 @@ export default function HeaderMobile() {
           </span>{"Certificates"}
         </Link>
         <Link
-          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] hover:bg-[#F4C9A5] dark:bg-[#1D1D1D] dark:hover:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)] hover:shadow-none", location.pathname.indexOf("/blogs") === 0 && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
+          className={classnames("menu-item text-black dark:hover:text-[white] dark:text-[white] bg-[#F3F6F6] dark:bg-[#1D1D1D] shadow-[0_4px_4px_0px_rgba(0,0,0,0.2)]", location.pathname.indexOf("/blogs") === 0 && "bg-[#F4C9A5] dark:bg-gradient-to-l from-[#df0000] to-[#550089] shadow-none")}
           to="./blogs"
         >
           <span className="text-[1rem] mb-1">
